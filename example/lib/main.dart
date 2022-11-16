bb76182608558a3b307907445191d44aa36d0f9d
Ömer Güven <omr.gven@gmail.com>
2022-11-16 13:21:14 +0300

    update example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -12,19 +12,8 @@ class ExampleApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+      title: 'Marquee Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: const AppHome(),
     );
   }
@@ -52,18 +41,18 @@ class _AppHomeState extends State<AppHome> {
               pps: 100,
               controller: controller,
               onChangeItemInViewPort: (index) {
-                print(index);
+                print('item index: $index');
               },
               onInteraction: () {
-                print('interaction callback');
+                print('on interaction callback');
               },
               onStarted: () {
-                print('marquee started');
+                print('on started callback');
               },
               onStoped: () {
-                print('marquee stoped');
+                print('on stopped callback');
               },
-              child: Text(
+              child: const Text(
                 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pretium massa mollis lorem blandit imperdiet. Nulla mattis vitae mauris vel condimentum. Nam posuere, augue vitae lobortis consequat, odio ante condimentum est, at maximus augue purus id metus. Curabitur condimentum aliquet ante at aliquet. Quisque vel massa congue, bibendum leo sodales, malesuada ante. Maecenas sed tortor quis ipsum dictum sollicitudin.',
               ),
             ),
@@ -74,7 +63,7 @@ class _AppHomeState extends State<AppHome> {
               interaction: false,
               controller: controller,
               direction: MarqueeDirection.rtl,
-              child: Text(
+              child: const Text(
                 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pretium massa mollis lorem blandit imperdiet. Nulla mattis vitae mauris vel condimentum. Nam posuere, augue vitae lobortis consequat, odio ante condimentum est, at maximus augue purus id metus. Curabitur condimentum aliquet ante at aliquet. Quisque vel massa congue, bibendum leo sodales, malesuada ante. Maecenas sed tortor quis ipsum dictum sollicitudin.',
               ),
             ),
@@ -105,11 +94,11 @@ class _AppHomeState extends State<AppHome> {
             children: [
               TextButton(
                 onPressed: controller.stop,
-                child: Text("Stop"),
+                child: const Text('Stop'),
               ),
               TextButton(
                 onPressed: controller.start,
-                child: Text("Start"),
+                child: const Text('Start'),
               ),
             ],
           )
