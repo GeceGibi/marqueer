473e44e996a1d315e997b476809f1d97dc962116
Ömer Güven <omr.gven@gmail.com>
2022-11-17 13:46:32 +0300

    update package name

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,2 @@
 ## 0.0.1
 initial release.
-
-
-## 0.0.2
-add interactionEnabled method to MarqueeController.
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_marquee/flutter_marquee.dart';
+import 'package:marquee/marquee.dart';
 
 void main() {
   runApp(const ExampleApp());
